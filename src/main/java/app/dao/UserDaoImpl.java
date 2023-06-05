4297fba7fb291fb408de134d6ab9b997e8dac71e
Alejandro794 <c0lt2019@yandex.ru>
2023-06-05 14:25:43 +0300

    Initial commit

---
+++ src/main/java/app/dao/UserDaoImpl.java
@@ -0,0 +1,44 @@
+package app.dao;
+
+import app.models.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<User> getUserS() {
+        return manager.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return manager.find(User.class, id);
+    }
+
+    @Override
+    public void createNewUser(User user) {
+        manager.persist(user);
+    }
+
+    @Override
+    public void updateUser(Long id, User newUser) {
+        User oldUser = manager.find(User.class, id);
+        oldUser.setName(newUser.getName());
+        oldUser.setSurname(newUser.getSurname());
+        oldUser.setDate(newUser.getDate());
+        oldUser.setEmail(newUser.getEmail());
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        manager.remove(manager.find(User.class, id));
+    }
+}
---
+++ src/main/java/app/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package app.service;
+
+import app.dao.UserDao;
+import app.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> getUserS() {
+        return userDao.getUserS();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public User getUser(Long id) {
+        return userDao.getUser(id);
+    }
+
+    @Transactional
+    @Override
+    public void createNewUser(User user) {
+        userDao.createNewUser(user);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(Long id, User user) {
+        userDao.updateUser(id, user);
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(Long id) {
+        userDao.deleteUser(id);
+    }
+}
